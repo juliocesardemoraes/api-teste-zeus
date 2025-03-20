deb722d133df2e2637d97bd3bb16a209ba5e7319
juliocesardemoraes <jcmcf2012@gmail.com>
2025-03-20 10:11:03 -0300

    fix: remove last update quotes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 // Add your code here
 import express from "express";
 import cors from "cors";
-import { quotes } from "./quotes.js";
 
 const app = express();
 app.use(
@@ -14,10 +13,6 @@ app.use(express.json());
 // SETUP ROUTES
 app.get("/", (req, res) => {
   res.status(200).send({ test: true });
-});
-
-app.get("/quotes", (req, res) => {
-  res.status(200).send(quotes);
 });
 
 const PORT = process.env.PORT || 3000;
